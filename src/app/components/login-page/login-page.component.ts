a59b86af5aa38cc2229bcfcd51866fec7e5b89c9
Qasim Khan <56044350+Qasimkhan786@users.noreply.github.com>
2023-02-16 00:29:21 +0500

    improve

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{GetuserdataService} from "../../services/getuserdata.service";
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor() { }
-
+  val:any;
+  constructor(private getbrowser:GetuserdataService) {
+    this.val=getbrowser.getBrowserName();
+    console.log(this.val);
+  }
+  
   ngOnInit(): void {
   }
+  
 
 }
--- src/app/services/getuserdata.service.ts
+++ src/app/services/getuserdata.service.ts
@@ -17,4 +17,25 @@ export class GetuserdataService {
    senddata(senddata:any){
     return this.http.post(this.urlsend,senddata)
    }
+   getBrowserName(): string {
+    /* Storing user's device details in a variable*/
+    let details = navigator.userAgent;
+    var val:any='';
+    /* Creating a regular expression
+    containing some mobile devices keywords
+    to search it in details string*/
+    let regexp = /android|iphone|kindle|ipad/i;
+    
+    /* Using test() method to search regexp in details
+    it returns boolean value*/
+    let isMobileDevice = regexp.test(details);
+    
+    if (isMobileDevice) {
+       val="Mobile";
+    } else {
+       val="Desktop";
+    }
+
+    return val;
+  }
 }
