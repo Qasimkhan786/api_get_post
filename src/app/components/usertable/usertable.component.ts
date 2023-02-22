3221fe96ad4e84d1d7ccef85074e66733cc831c1
Qasim Khan <56044350+Qasimkhan786@users.noreply.github.com>
2023-02-22 23:12:20 +0500

    button update

--- src/app/components/usertable/usertable.component.ts
+++ src/app/components/usertable/usertable.component.ts
@@ -15,7 +15,7 @@ export class UsertableComponent implements OnInit {
   btnda="medium1btn medactive";
   outline="foroutline"
   val=true;
-  
+  fordaysactive="active";
   // val1=false;
   component1(){
     if(this.val){
@@ -26,6 +26,78 @@ export class UsertableComponent implements OnInit {
   component2(){
     if(!this.val){
       this.val=true;
+    }
+  }
+  mon=true;
+  tue=false;
+  wed=false;
+  th=false;
+  fri=false;
+  sat=false;
+  sun=false;
+  fordaysbtn(value:any){
+    if(value=="mon"){
+      this.mon=true;
+      this.tue=false;
+      this.wed=false;
+      this.th=false;
+      this.fri=false;
+      this.sat=false;
+      this.sun=false;
+    }
+    else if(value=="tue"){
+      this.mon=false;
+      this.tue=true;
+      this.wed=false;
+      this.th=false;
+      this.fri=false;
+      this.sat=false;
+      this.sun=false;
+    }
+    else if(value=="wed"){
+      this.mon=false;
+      this.tue=false;
+      this.wed=true;
+      this.th=false;
+      this.fri=false;
+      this.sat=false;
+      this.sun=false;
+    }
+    else if(value=="th"){
+      this.mon=false;
+      this.tue=false;
+      this.wed=false;
+      this.th=true;
+      this.fri=false;
+      this.sat=false;
+      this.sun=false;
+    }
+    else if(value=="fri"){
+      this.mon=false;
+      this.tue=false;
+      this.wed=false;
+      this.th=false;
+      this.fri=true;
+      this.sat=false;
+      this.sun=false;
+    }
+    else if(value=="sat"){
+      this.mon=false;
+      this.tue=false;
+      this.wed=false;
+      this.th=false;
+      this.fri=false;
+      this.sat=true;
+      this.sun=false;
+    }
+    else if(value=="sun"){
+      this.mon=false;
+      this.tue=false;
+      this.wed=false;
+      this.th=false;
+      this.fri=false;
+      this.sat=false;
+      this.sun=true;
     }
   }
 }
